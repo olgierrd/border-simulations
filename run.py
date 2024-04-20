575efd6ce024b77426f12260481ce9095ca55450
olgierrd <oolgierrd@gmail.com>
2024-04-20 18:02:21 +0200

    run: prints out distribution of drugs among policeman and count of steps

--- run.py
+++ run.py
@@ -1,23 +1,25 @@
-import seaborn as sns
 import numpy as np
+
+import agents
 from model import DrugsModel
 import matplotlib.pyplot as plt
 
 # Create a model with 10 agents
-model = DrugsModel(3, 10, 20, 20)
-for _ in range(1):
+model = DrugsModel(8, 5, 30, 30)
+steps = 0
+
+# Run the model while there are still smugglers
+while any(isinstance(agent, agents.SmugglerAgent) for agent in model.schedule.agents):
     model.step()
+    steps += 1
 
-# Create a 2D numpy array representing the grid
-grid = np.zeros((model.grid.width, model.grid.height))
 
-# Fill the array with the number of smugglers and policemen in each cell
-for agent in model.schedule.agents:
-    x, y = agent.pos
-    grid[x][y] += 1
 
-# Plot the grid using seaborn
-plt.figure(figsize=(10, 10))
-sns.heatmap(grid, annot=True, fmt=".0f", cmap='viridis')
-plt.title('Number of agents on each cell of the grid')
-plt.show()
+# Get the number of drugs police agents have
+drugs = {f"{agent.__class__} with ID{agent.unique_id}": agent.drugs for agent in model.schedule.agents}
+
+print(steps, drugs)
+
+
+
+
